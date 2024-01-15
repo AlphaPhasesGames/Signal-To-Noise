a795cdec3f941ff5da7e80e755ed4e31a42f1a49
WHawke <gothreepwood@gmail.com>
2024-01-15 14:13:31 +0000

    A lot has been added and removed First edited the console so it was all one object, added and removed a few versions as you can see. Settled on a half console with screen. Changed the star background to look better edit the player controller, changes the way the player moved. This was actually due to a collision issue but editing it inproved my FPS by a significat amount

--- Assets/Signal To Noise/SCripts/RobotController.cs
+++ Assets/Signal To Noise/SCripts/RobotController.cs
@@ -9,55 +9,56 @@ namespace TUSOM.Alpha.Phases.Games
     public class RobotController : MonoBehaviour
     {
         [Header("Controller Scripts")]
-        public CharacterController charControl;
-        public bool canPlayerMove;
-      
+        private CharacterController charControl;
+
+        [SerializeField]
+        private float moveSpeed;
+        private float inputX;
+        private float inputZ;
+        private Vector3 v_movement;
+        private Vector3 v_velocity;
+
         public Camera mainCam; // declare camera to store our camera from the inspector
-        public Vector3 currentPostition; // a vector 3 to hold the players current position - for the navemesh
-                                         // public Vector3 transBrickposition;
         public Ray cameraRay; // public ray to detect when the player is over ground/brick etc
         RaycastHit cameraRayHit; // raycasthit to detect when the ray has hit something
         RaycastHit hit; // = Physics.Raycast (Camera.main.ScreenPointToRay(Input.mousePosition));
-        public Vector3 velocity; // instantiate a vector3 called velocity, to move the player down, to simulate gravity
 
-        private float move, movespeed, rotation, rotatespeed;
-
+        private void Awake()
+        {
+            charControl = GetComponent<CharacterController>();
+        }
 
         void Start()
         {
-        //    agent = GetComponent<NavMeshAgent>(); // get NavMeshAgent component and apply it to agent .. this is redundant, we have set it in the inspector 
-            canPlayerMove = true;
-            movespeed = 5f;
-            rotatespeed = 100f;
-          
+            moveSpeed = 5f;          
         }
 
         // Update is called once per frame
         void Update()
         {
+            inputX = Input.GetAxis("Horizontal");
+            inputZ = Input.GetAxis("Vertical");
 
-            move = Input.GetAxis("Vertical") * movespeed * Time.deltaTime;
-            rotation = Input.GetAxis("Horizontal") * rotatespeed * Time.deltaTime;
-           
-            
               if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()) // on mouse click
-                  {
-                      Ray position = mainCam.ScreenPointToRay(Input.mousePosition); // new RAY decalred as position and is set to the mouse position called once per frame
-                      RaycastHit hitInfo; // get a raycast variable to detect what we hit
+                    {
+                        Ray position = mainCam.ScreenPointToRay(Input.mousePosition); // new RAY decalred as position and is set to the mouse position called once per frame
+                        RaycastHit hitInfo; // get a raycast variable to detect what we hit
 
-                      if (Physics.Raycast(position, out hitInfo, 100f)) // if the raycast from position hits in distance of 100f
-                      {
-                          Debug.Log("We Hit: " + hitInfo.collider); // log the position it hits
-                      }
+                        if (Physics.Raycast(position, out hitInfo, 100f)) // if the raycast from position hits in distance of 100f
+                        {
+                            Debug.Log("We Hit: " + hitInfo.collider); // log the position it hits
+                        }
 
-                      Debug.Log(hit.collider); // log the collider it hits
-                  }
-            
+                        Debug.Log(hit.collider); // log the collider it hits
+                    }
         }
-        private void LateUpdate()
+
+        private void FixedUpdate()
         {
-            transform.Translate(0f, 0f, move);
-            transform.Rotate(0f, rotation, 0f);
+            v_movement = charControl.transform.forward * inputZ;
+            charControl.transform.Rotate(Vector3.up * inputX * (100f * Time.deltaTime));
+
+            charControl.Move(v_movement * moveSpeed * Time.deltaTime);
         }
     }
 }
